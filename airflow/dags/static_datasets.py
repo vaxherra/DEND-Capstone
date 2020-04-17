f0a1f6bfbba8f859e122ea4ef93c5f344377c7a3
Robert Kwapich <robert.kwapich@gmail.com>
2020-04-17 11:55:27 -0400

    adds docstrings

--- airflow/dags/static_datasets.py
+++ airflow/dags/static_datasets.py
@@ -23,8 +23,14 @@ from sql_statements import sql_statements
 
 def load_to_s3(location,filename,s3_bucket,s3_key,aws_credentials_id,*args,**kwargs):
     """
-    Load provided `file` in a `locaiton` to a given `s3_bucket` placing it in `s3_key` using `aws_credentials_id`.
+    A python helper function for Airflow PythonOperator: Load provided `file` in a `locaiton` to a given `s3_bucket` placing it in `s3_key` using `aws_credentials_id`.
 
+    Args:
+        location            : a directory location of a file to upload
+        filename            : the name of the file to upload to an S3 bucket
+        s3_bucket           : the name of the AWS S3 bucket
+        s3_key              : AWS S3 key of a specified bucket, i.e. a "directory"
+        aws_credentials_id  : Airflow connection ID for IAM user. Must have S3 read and WRITE access to a specified S3 bucket
     """
 
     s3_hook = S3Hook(aws_credentials_id)
@@ -47,42 +53,42 @@ def load_to_s3(location,filename,s3_bucket,s3_key,aws_credentials_id,*args,**kwa
 
 def load_data_to_redshift(target_table, s3_location,aws_credentials_id , *args, **kwargs):
     """
-    TODO: document, S3 -> Redshift
-    """
+    A simple PythonOperator helper function to transfer files from S3 to a target table in Redshift/Posgres.
 
-    # Obtain AWS S3 access credentials
+    Args:
+        target_table                : name of the target table in Redshift/Postgres database,
+        s3_location                 : the location of a file in S3 bucket
+        aws_credentials_id          : Airflow connection ID for AWS IAM user. Must have a read access to provided S3 bucket.
+    """
+    # Obtain AWS S3 access credentials using AwsHook
     aws_hook = AwsHook(aws_credentials_id)
     credentials = aws_hook.get_credentials()
-    # Obtain Redshift credentials
+    # Obtain Redshift credentials using PostgresHook
     redshift_hook = PostgresHook("redshift")
-
 
     copy_sql = (sql_statements.COPY_SQL.format(
         target_table, 
         s3_location,
         credentials.access_key, credentials.secret_key
         ))
-    print(copy_sql)
+    # Run copy SQL statement
     redshift_hook.run(  copy_sql )
 
 ##########################################################################
- 
 # Default parameters
 default_args = {
     'owner': 'Robert Kwapich',
     'start_date': datetime(2020, 3, 29),
-    'depends_on_past': False, # no need to run sequentially in time for GDELT data, can parallelize tasks
+    'depends_on_past': False, 
     'retries': 3,
     'retry_delay': timedelta(minutes=5),
     'catchup': False, 
     'email_on_retry': False
 }
 
-# TODO: fill DAG description
-
 dag = DAG(
     dag_id='static_dataset',
-    description = "A set of ETL procedures for static datasets: GNIS ...",
+    description = "A set of ETL procedures for static datasets: GNIS.",
     default_args=default_args,
     schedule_interval= "@once", #  once, and only once
     dagrun_timeout=timedelta(minutes=60), # how long a DagRun should be up before timing out / failing,
@@ -90,14 +96,13 @@ dag = DAG(
     max_active_runs=1
 )
 
-
 #############################################
 ######### DEFINE A SET OF ETL TASKS #########
 #############################################
-
+# A dummy operator, for ease of interpretability
 start_operator = DummyOperator(task_id='Begin_execution',  dag=dag)
 
-# Creates a set of empty tables
+# Create set set of TABLES in target Redshift Cluster / Postgres dtabase
 create_tables = PostgresOperator(
     task_id="create_redshift_tables",
     dag=dag,
@@ -106,8 +111,7 @@ create_tables = PostgresOperator(
 )
 
 ############# Load files to S3 
-
-# Load file to S3
+# Load GNIS database file to S3
 gnis_to_s3 = PythonOperator(
     task_id='gnis_to_s3',
     dag=dag,
@@ -122,8 +126,7 @@ gnis_to_s3 = PythonOperator(
     }
 )
 ############# Load S3 -> Staging Tables
-
-# S3 -> staging table 'gnis_staging'
+# Load GNIS table to a staging table in Redshift
 staging_gnis_2_redshift  = PythonOperator(
     task_id='staging_gnis_2_redshift',
     dag=dag,
@@ -134,11 +137,7 @@ staging_gnis_2_redshift  = PythonOperator(
     'aws_credentials_id':'aws_credentials'}, 
 )
 
-############# Dimensional modelling
-# ? 
-
-
-# TODO: gnis_staging to gnis
+############# Staging Table -> Dimension Table
 load_gnis_dim_table = stage2table(
 task_id='load_gnis_dim_table',
 dag=dag,
@@ -151,6 +150,8 @@ query=sql_statements.gnis_table_insert
 
 )
 
+
+############# Run data quality checks:
 run_quality_checks = DataQualityOperator(
     task_id='run_data_quality_checks',
     dag=dag,
@@ -161,6 +162,8 @@ run_quality_checks = DataQualityOperator(
 )
 
 
+############# Drop entire staging table
+# Delete staging table - saves space and operational costs of storing data
 drop_gnis_staging = PostgresOperator(
     task_id="drop_gnis_staging",
     dag=dag,
@@ -168,18 +171,13 @@ drop_gnis_staging = PostgresOperator(
     sql='DROP TABLE gnis_staging'  
 )
 
-
-
-
+############ Dummpy operator indicating END of a DAG
 end_operator = DummyOperator(task_id='End_execution',  dag=dag)
-
-
 
 #############################################
 ##### BUILDING A DAG ORDER DEPENDENCIES #####
 #############################################
-
-
+# A basic sequential DAG
 start_operator >> create_tables
 create_tables >> gnis_to_s3
 gnis_to_s3 >> staging_gnis_2_redshift
