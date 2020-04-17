7bd1251e02820921712dab297cc5feb3e5469a0d
Robert Kwapich <robert.kwapich@gmail.com>
2020-04-17 11:55:35 -0400

    adds docstrings

--- airflow/dags/GDELT_stream.py
+++ airflow/dags/GDELT_stream.py
@@ -13,7 +13,7 @@ from airflow.operators.python_operator import PythonOperator
 
 from airflow.contrib.hooks.aws_hook import AwsHook
 from airflow.hooks.postgres_hook import PostgresHook
-
+from airflow import AirflowException
 import wget
 import pytz
 
@@ -22,46 +22,48 @@ from sql_statements import sql_statements
 #############################################
 ######### Python functions for ETL ##########
 #############################################
-
-def redshift_date_delete_rows(redshift_conn_id, *args,**kwargs):
+#   Functions for defined PythonOperator's  #
+def redshift_date_delete_rows(target_table, redshift_conn_id, *args,**kwargs):
     """
-    TODO: document
+    A simple helper function to delete rows in a 'target_table' templated by context variable 'ds', i.e. the day of DAG execution formatted as 'YYYY-MM-DD'.
+    Uses provided 'redshift_conn_id' airflow connection ID to establish a connection to redshift.
 
+    Args:
+        target_table            : the name of the target table in Redshift/Postgres
+        redshift_conn_id        : an Airflow connection ID for AWS Redshift ("Postgres" type) connection,
     """
-
     redshift_hook = PostgresHook(redshift_conn_id)
 
-    delete_sql = "DELETE FROM staging_gdelt_events WHERE sqldate={}"  .format(kwargs['ds'])
-
+    delete_sql = "DELETE FROM {} WHERE sqldate={}"  .format(target_table,kwargs['ds'])
     try:
         redshift_hook.run(delete_sql)
-
     except:
         print("Could not run the DELETE SQL statement")
         AirflowException("Could not run the DELETE SQL statement")
 
-
-
-
 def load_data_to_redshift(aws_credentials_id,redshift_conn_id,target_table,url_base,*args, **kwargs):
     """
-    TODO: document
-    """
+    A helper function to facilitate transfer of files from S3 bucket to a specified redshift table.
 
+    Args:
+        aws_credentials_id              : an Airflow connection ID for AWS IAM user (requires S3 read access),
+        redshift_conn_id                : an Airflow connection ID for Redshift/Posgres database,
+        target_table                    : name of a Redshift/Posgres table, where the data from S3 will be placed,
+        url_base                        : address of S3 bucket with three templated fields (marked with {}) that will be filled with date YYYY, MM, and YYYYMMDD.
+    """
     # Obtain AWS S3 access credentials
     aws_hook = AwsHook(aws_credentials_id)
     credentials = aws_hook.get_credentials()
     # Obtain Redshift credentials
     redshift_hook = PostgresHook(redshift_conn_id)
- 
-
+    # Templating SQL copy statement with Airflow macro: date of execution in YYYYMMDD format
     copy_sql = (sql_statements.COPY_SQL_GZIP.format(
         target_table, 
         url_base.format(kwargs['ds_nodash'][:4], kwargs['ds_nodash'][4:6], kwargs['ds_nodash'] ),
         credentials.access_key, credentials.secret_key
         ))
-    print(copy_sql)
 
+    # Running the copy statement
     try:
         redshift_hook.run(  copy_sql )
     except:
@@ -69,10 +71,10 @@ def load_data_to_redshift(aws_credentials_id,redshift_conn_id,target_table,url_b
         AirflowException("Could not run the COPY SQL statement")
 
 #############################################
-# Default parameters
+# Default parameters for a specified DAG
 default_args = {
     'owner': 'Robert Kwapich',
-    'start_date': datetime(2020, 4, 12),
+    'start_date': datetime(2020, 1, 1),
     'end_date': datetime(2020, 4, 30),
     'depends_on_past': False, # no need to run sequentially in time for GDELT data, can parallelize tasks
     'retries': 3,
@@ -99,8 +101,10 @@ dag = DAG(
 ######### DEFINE A SET OF ETL TASKS #########
 #############################################
 
+# START: a dummy placeholder operator - for visibility
 start_operator = DummyOperator(task_id='Begin_execution',  dag=dag)
  
+# DOWNLOAD AND PUSH DATA TO S3
 source_to_s3 = GDELT2S3(
     task_id="GDELT_to_s3",
     dag=dag,
@@ -112,7 +116,7 @@ source_to_s3 = GDELT2S3(
     provide_context=True
 )
 
-
+# CREATE (or do nothing if already exist) A SET OF TARGET TABLES
 create_tables = PostgresOperator(
     task_id="create_redshift_tables",
     dag=dag,
@@ -120,7 +124,7 @@ create_tables = PostgresOperator(
     sql='/sql_statements/create_tables.sql'  
 )
 
-
+# COPY DATA FROM S3 -> Redshift staging table
 staging_gdelt = PythonOperator(
     task_id='stage_from_s3_to_redshift',
     dag=dag,
@@ -132,7 +136,7 @@ staging_gdelt = PythonOperator(
     'redshift_conn_id':'redshift'}, 
 )
 
- 
+# COPY SELECTED DATA FROM STAGING TABLE TO FACT TABLE
 load_gdelt_fact_table = stage2table(
     task_id='load_gdelt_fact_table',
     dag=dag,
@@ -144,6 +148,8 @@ load_gdelt_fact_table = stage2table(
     query=sql_statements.gdelt_events_table_insert 
 
 )
+
+# RUN DATA QUALITY CHECKS
 run_quality_checks = DataQualityOperator(
     task_id='run_gdelt_quality_checks',
     dag=dag,
@@ -153,36 +159,30 @@ run_quality_checks = DataQualityOperator(
           ]
 )
 
-
+# CLEAR STAGING TABLE for a selected day only, as there might be multiple concurrent processes running
 clearing_staging_gdelt_events = PythonOperator(
     task_id='clearing_staging_gdelt_events',
     dag=dag,
 
     python_callable=redshift_date_delete_rows,
     provide_context=True,
-    op_kwargs={'redshift_conn_id':'redshift'}, 
+    op_kwargs={'redshift_conn_id':'redshift', 
+    'target_table':'staging_gdelt_events'}, 
 )
 
-
-
-# TODO: data quality operator
-# no nulls in ID
-# some records
-# no missing dates
-
-
-
+# DUMMY END OPERATOR - for visibility only
 end_operator = DummyOperator(task_id='End_execution',  dag=dag)
 
 #############################################
 ##### BUILDING A DAG ORDER DEPENDENCIES #####
 #############################################
+# Building a DAG of operations for consistency:
+#  a simple linear DAG that can be parallelized
+
 start_operator >> source_to_s3
 source_to_s3 >> create_tables
 create_tables >> staging_gdelt
 staging_gdelt >> load_gdelt_fact_table
 load_gdelt_fact_table>> run_quality_checks
-
-# TODO: data quality operator
 run_quality_checks >> clearing_staging_gdelt_events
 clearing_staging_gdelt_events >> end_operator
